7d5e9ab3e107fe2648d1ac3f58b6ea0a7ef66f52
Shivansh Srivastava <srivastavashiv0112@gmail.com>
2022-08-20 23:17:24 +0530

    CI-CD established

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app=Flask(__name__)
 
 @app.route('/',methods=['GET','POST'])
 def index():
-    return("Demo Page")
+    return("CI CD established")
 
 if __name__=="__main__":
     app.run(debug=True)
