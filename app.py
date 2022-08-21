66346c36859f688692c47fef5cb05143e98b4df4
Shivansh Srivastava <srivastavashiv0112@gmail.com>
2022-08-21 23:25:48 +0530

    logging

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app=Flask(__name__)
 @app.route('/',methods=['GET','POST'])
 def index():
     logging.info("Testing logging")
-    return("CI CD established")
+    return("Logging")
 
 if __name__=="__main__":
     app.run(debug=True)
