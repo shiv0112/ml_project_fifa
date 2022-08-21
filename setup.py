51fe4d202d6371965313941ffb1493be3a5a6420
Shivansh Srivastava <srivastavashiv0112@gmail.com>
2022-08-21 22:32:08 +0530

    logging"

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
-from setuptools import setup  
+from setuptools import setup,find_packages
 from typing import List
 
 PROJECT_NAME="fifa-rating"
-VERSION="1.0.0"
+VERSION="0.0.2"
 AUTHOR="Shivansh Srivastava"
 DESCRIPTION="THis is a full FDSD project to predict fifa overall rating"
-PACKAGES=["fifa_rating"]
 REQUIREMENTS_FILE_NAME="requirements.txt"
 
 def get_requirements_list() -> List[str]:
@@ -13,15 +12,24 @@ def get_requirements_list() -> List[str]:
     Description: This function is going to return a list which contains name of 
     libraries mentioned in our requirements.txt file.
     """
-    with open(REQUIREMENTS_FILE_NAME) as requirements_file:
-        return requirements_file.readlines()
+    with open('requirements.txt') as f:
+        packages = []
+        for line in f:
+            line = line.strip()
+            # let's also ignore empty lines and comments
+            if not line or line.startswith('#'):
+                continue
+            if '-e .' in line:
+                line = ''
+            packages.append(line)
+    return packages
 
 setup(
     name=PROJECT_NAME,
     version=VERSION,
     author=AUTHOR,
     description=DESCRIPTION,
-    packages=PACKAGES,
+    packages=find_packages(),#PACKAGES=["fifa_rating"]
     install_requires=get_requirements_list()
 )
 
