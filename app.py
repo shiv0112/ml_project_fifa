9d0c18ce384b5bb65bb76dbdd3d1dcf09d6aff53
Shivansh Srivastava <srivastavashiv0112@gmail.com>
2022-11-29 02:27:55 +0530

    model created

--- app.py
+++ app.py
@@ -1,17 +1,47 @@
-from flask import Flask
-import sys
+from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS,cross_origin
+import numpy as np
+from pickle import load
 
 app=Flask(__name__)
 
-@app.route('/',methods=['GET','POST'])
+@app.route('/',methods=['GET'])
+@cross_origin()
+def homePage():
+    return render_template("index.html")
+
+@app.route('/predict',methods=['GET','POST'])
+@cross_origin()
 def index():
     try:
-        raise Exception("Manually invoked an exception")
-    except Exception as e:
-        fifa = FifaException(e,sys)
-        logging.info(fifa.error_message)
-        logging.info("Successfully tested logger")
-    return "Logging and Exception done"
+            #  reading the inputs given by the user
+            potential=float(request.form['potential'])
+            finishing = float(request.form['finishing'])
+            heading_accuracy = float(request.form['heading_accuracy'])
+            dribbling = float(request.form['dribbling'])
+            ball_control = float(request.form['ball_control'])
+            reactions = float(request.form['reactions'])
+            shot_power = float(request.form['shot_power'])
+            strength = float(request.form['strength'])
+            aggression = float(request.form['aggression'])
+            marking = float(request.form['marking'])
+            gk_diving = float(request.form['gk_diving'])
+            gk_kicking = float(request.form['gk_kicking'])
+            gk_positioning = float(request.form['gk_positioning'])
 
-if __name__=="__main__":
+            x_input=[potential,finishing,heading_accuracy,dribbling,ball_control,reactions,shot_power,strength,aggression,marking,gk_diving,gk_kicking,gk_positioning]
+            
+            filename = 'model.pkl'
+            loaded_model = load(open(filename, 'rb')) # loading the model file
+            # predictions using the loaded model file
+            
+            prediction=loaded_model.predict([x_input])[0]
+
+            # showing the prediction results in a UI
+            return render_template('results.html',prediction=np.ceil(prediction))
+    except Exception as e:
+            print('The Exception message is: ',e)
+            return 'Something went wrong'
+
+if __name__ == "__main__":
     app.run(debug=True)
